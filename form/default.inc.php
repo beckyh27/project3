4ce6b03532fd8310124460beb9ec3ed92674d2e1
Becky Hassler <rebeccahassler@ufl.edu>
2020-12-08 22:46:27 -0500

    added form processing

---
+++ form/default.inc.php
@@ -0,0 +1,14 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html/#home"><img src="../images/logo.png" alt="logo" title="Logo"></a></li>
+      <li><a href="index.html#work">Work</a></li>
+      <li><a href="index.html#about">About</a></li>
+      <li><a href="index.html#contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+<div class="container">
+  <h1>Sorry something went wrong with your submission</h1>
+  <p>Please try again</p>
+</div>
